cbac234d5fd7468bae46e25a11402e0231be808e
codedbyyashwanth <61375676+codedbyyashwanth@users.noreply.github.com>
2021-08-09 13:31:24 +0530

    Initial Commit

---
+++ navbar.php
@@ -0,0 +1,38 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <a href="index.php" class="logo me-auto"><img src="./assets/img/logo.png" alt="" class="Logo"></a>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a id="home" href="./index.php">Home</a></li>
+          <li><a id="about" href="./about.php">About</a></li>
+          <li><a id="gallery" href="./gallery.php">Gallery</a></li>
+          <li><a id="event" href="./events.php">Events</a></li>
+          <li><a id="courses" href="./courses.php">Courses</a></li>
+          <li><a id="contact" href="./contact.php">Contact</a></li>
+          <li class="dropdown">
+            <a href="#"><span>Resource</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li>
+                <li><a id="admission" href="./admission.php">Admission InQuery</a></li>
+                <li><a id="study-material" href="./study-material.php">Study Material</a></li>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#"><span>Fields</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li>
+                <li><a id="pu" href="./pu.php">PU</a></li>
+                <li><a id="degree" href="./degree.php">Degree</a></li>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+      <a href="courses.php" class="get-started-btn">Get Started</a>
+
+    </div>
+  </header>
